A few bug fixes with units
Also added a new output file of the final calibrated data for visualization.

khan/analysis/data_retrieval.py
@@ -26,7 +26,8 @@ class DataSubsection:
             The location of the reduced data FITS files.
         """
         self._wavelengths = wavelengths
-        self._average_wavelength = np.mean(self._wavelengths)
+        self._average_wavelength = np.mean([wavelength.value for wavelength
+                                            in wavelengths])
         self._science_observations_file = Path(reduced_data_path,
                                                'science_observations.fits.gz')
         self._observation_datetimes = self._get_observation_datetimes()
@@ -192,23 +193,23 @@ class DataSubsection:
         return self._wavelengths
 
     @property
-    def average_wavelength(self) -> float:
-        return self._average_wavelength.squeeze()
+    def average_wavelength(self) -> u.Quantity:
+        return self._average_wavelength.squeeze() * u.nm
 
     @property
-    def rest_wavelength_centers(self) -> np.ndarray:
+    def rest_wavelength_centers(self) -> u.Quantity:
         return self._rest_wavelength_centers[self._center_indices]
 
     @property
-    def shifted_wavelength_centers(self) -> np.ndarray:
+    def shifted_wavelength_centers(self) -> u.Quantity:
         return self._shifted_wavelength_centers[self._center_indices]
 
     @property
-    def rest_wavelength_edges(self) -> np.ndarray:
+    def rest_wavelength_edges(self) -> u.Quantity:
         return self._rest_wavelength_centers[self._edge_indices]
 
     @property
-    def shifted_wavelength_edges(self) -> np.ndarray:
+    def shifted_wavelength_edges(self) -> u.Quantity:
         return self._shifted_wavelength_centers[self._edge_indices]
 
     @property

khan/analysis/flux_calibration.py
@@ -90,12 +90,12 @@ class FluxCalibration:
         # calculate photon energy
         photon_energy = (c.h * c.c /
                          (self._data_subsection.shifted_wavelength_centers
-                          * u.nm * u.photon))
+                          * u.photon))
         photon_energy = photon_energy.to(u.J/u.photon)
 
         # interpolate solar spectrum over wavelengths
         solar_spectrum = \
-            np.interp(self._data_subsection.shifted_wavelength_centers,
+            np.interp(self._data_subsection.shifted_wavelength_centers.value,
                       spectral_radiance_data['wavelength'].value,
                       spectral_radiance_data['radiance'].value)
         solar_spectrum *= spectral_radiance_data['radiance'].unit
@@ -105,7 +105,7 @@ class FluxCalibration:
 
         # interpolate Jupiter meridian reflectivity over wavelengths
         reflectivity = \
-            np.interp(self._data_subsection.shifted_wavelength_centers,
+            np.interp(self._data_subsection.shifted_wavelength_centers.value,
                       reflectivity_data['wavelength'],
                       reflectivity_data['reflectivity'])
 
@@ -128,7 +128,8 @@ class FluxCalibration:
         """
         Calculate the wavelength dispersion (nm/bin).
         """
-        dispersion = np.diff(self._data_subsection.shifted_wavelength_edges)
+        dispersion = np.diff(
+            self._data_subsection.shifted_wavelength_edges.value)
         return np.tile(dispersion, (self._theoretical_flux.shape[0], 1)) * u.nm
 
     @property